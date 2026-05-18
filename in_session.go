45babb463dc6eae8f207c0ac0f48142338b5bb6e
Oscar Bohlin <oscar@bohlin.se>
2026-05-18 11:28:49 +0200

    fix meres

--- in_session.go
+++ in_session.go
@@ -264,11 +264,7 @@ func (state inSession) resendMessages(session *session, beginSeqNo, endSeqNo int
 		}
 
 		session.log.OnEventf("Resending Message: %v", sentMessageSeqNum)
-<<<<<<< HEAD
 		msgBytes = msg.buildWithBodyBytes(msg.bodyBytes) // workaround for maintaining repeating group field order
-=======
-		msgBytes = msg.buildFromBodyBytes()
->>>>>>> origin/master
 		session.EnqueueBytesAndSend(msgBytes)
 
 		seqNum = sentMessageSeqNum + 1
